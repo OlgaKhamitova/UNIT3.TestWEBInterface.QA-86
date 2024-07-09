613375e8753d7dce38120d60bc7beb5a46471e05
Olga Khamitova <olgacherkasova13317@rambler.ru>
2024-07-09 11:13:11 +0300

    Tests are added and fixed

--- src/test/java/CardRequestTest.java
+++ src/test/java/CardRequestTest.java
@@ -1,13 +1,14 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
 public class CardRequestTest {
     @Test
-    public void test_CardRequest() {
+    public void testCardRequest() {
         open("http://localhost:9999/");
         $("[data-test-id=name] input").setValue("Хамитова Ольга");
         $("[data-test-id=phone] input").setValue("+79991111111");
@@ -18,12 +19,38 @@ public class CardRequestTest {
     }
 
     @Test
-    public void test_ValidationCallbackForm() {
+    public void testValidationCallbackFormInvalidName() {
         open("http://localhost:9999/");
         $("[data-test-id=name] input").setValue("KhamitovaOlga");
+        $("[data-test-id=phone] input").setValue("+79991111111");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+    @Test
+    public void testValidationCallbackFormInvalidPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Хамитова Ольга");
         $("[data-test-id=phone] input").setValue("+7999111111");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("#root > div > form > div:nth-child(1) > span > span > span.input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+    @Test
+    public void testValidationCallbackFormEmpty() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+    @Test
+    public void testCardRequestWithoutCheckbox() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Хамитова Ольга");
+        $("[data-test-id=phone] input").setValue("+79991111111");
+        $("button").click();
+        $("[data-test-id=agreement]").shouldBe(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"), visible);
     }
 }
